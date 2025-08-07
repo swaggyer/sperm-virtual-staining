9e629c6fee83aae2379f71298671e4e6f913ba68
嘉浩 <jiahaow77@163.com>
2025-08-07 23:27:32 +0800

    feat:计算网络参数量

--- val_netnumber.py
+++ val_netnumber.py
@@ -3,8 +3,8 @@ from G_network.SPPF_UNet import *
 
 
 
-results_file = "output/superunet2/checkpoint.txt"
-generator_path = r"superunet2/best_generator.pth"
+# results_file = "output/superunet2/checkpoint.txt"
+generator_path = "saved_models/SPPF777_DSUNet/best_generator.pth"
 # 加载模型
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # generator = ResNetL3(3)# 确保类名正确
@@ -22,8 +22,8 @@ total_params = sum(p.numel() for p in generator.parameters())
 print(f"Total trainable parameters: {total_params}")
 
 
-with open(results_file, "a") as f:
-    train_info = f"参数量约为{total_params}。\n"
-    f.write(train_info + "\n\n")
+# with open(results_file, "a") as f:
+#     train_info = f"参数量约为{total_params}。\n"
+#     f.write(train_info + "\n\n")
 
 print("处理完成")
